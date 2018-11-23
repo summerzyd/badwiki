161485adeabe60143521750d8df2e15494d0b177
root <root@localhost.localdomain>
2018-11-23 14:50:54 +0800

    init the project based by springboot

---
+++ src/main/java/com/github/badwiki/badwiki/BadwikiApplication.java
@@ -0,0 +1,12 @@
+package com.github.badwiki.badwiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadwikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BadwikiApplication.class, args);
+	}
+}
